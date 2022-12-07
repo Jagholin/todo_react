553a1273cda8cdcadddc06cbd8c3adf13fd38a17
Jagholin <pavel.belskiy2@gmail.com>
2022-12-07 13:04:29 +0100

    create buttons for edit and delete

--- src/App.js
+++ src/App.js
@@ -15,11 +15,41 @@ function App() {
     {
       todo: "create a todo app",
       done: false,
-    }
+    },
   ]);
 
   // index of currently selected item
   const [selected, setSelected] = useState(0);
+
+  const handleTaskDelete = () => {
+    if (selected >= state.length) {
+      console.log("selected element index out of bounds");
+      return;
+    }
+
+    setState(state.filter((_, ind) => ind !== selected));
+  };
+
+  const handleTaskEdit = () => {
+    if (selected >= state.length) {
+      console.log("selected element index out of bounds");
+      return;
+    }
+    let newTodo = prompt("Edit todo task description:", state[selected].todo);
+    if (newTodo === null) {
+      return; // User pressed cancel
+    }
+    if (newTodo.length > 0) {
+      setState(
+        state.map((val, ind) =>
+          ind !== selected ? val : { todo: newTodo, done: false }
+        )
+      );
+    } else {
+      // Treat 0-length input as erasing the item
+      handleTaskDelete();
+    }
+  };
 
   return (
     <div className="App">
@@ -27,7 +57,11 @@ function App() {
       <div>
         <div>
           <NewTask />
-          <Buttons />
+          <Buttons
+            state={state}
+            onTaskEdit={handleTaskEdit}
+            onTaskDelete={handleTaskDelete}
+          />
         </div>
         <TodoList />
       </div>
--- src/components/buttons.jsx
+++ src/components/buttons.jsx
@@ -1,5 +1,15 @@
-function Buttons(props) {
-    return ( <div></div> );
+import "./buttons.css";
+function Buttons({ onTaskEdit, onTaskDelete }) {
+  return (
+    <div className="buttons">
+      <button className="button" onClick={onTaskEdit}>
+        Edit Task
+      </button>
+      <button className="button" onClick={onTaskDelete}>
+        Delete Task
+      </button>
+    </div>
+  );
 }
 
 export default Buttons;
