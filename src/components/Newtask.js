81bc23d81bea6927899108090d6bea8df8406c60
Aleksandra Marczewska <117514059+marczewska@users.noreply.github.com>
2022-12-07 17:49:07 +0100

    imput section with functionality

--- src/components/Newtask.js
+++ src/components/Newtask.js
@@ -1,13 +1,37 @@
 import "./newtask.css"
-import { useState} from "react"
-function NewTask({state}) {
-    const [todo, setTodo]  = useState("")
+import { useState, useRef } from "react"
+
+function NewTask({}) {
+
+    const [todo, setTodo]  = useState([])
+    const todoElementRef = useRef()
+
+    function handleAddTodo(e){
+        const element = todoElementRef.current.value
+        todoElementRef.current.focus();
+        if (element === '') return
+        setTodo(prevTodo => {
+            return [...prevTodo, {id: {todo}, name: element}]
+        })
+        console.log(element)
+        todoElementRef.current.value = null
+    }
+    
+    function handleKeyDown(e) {
+        let key = e.key;
+        if (e.key === 'Enter'){
+        handleAddTodo();   
+        }
+    }
+
     return ( 
         <div className= "newTask">
             <h2 className="title">Enter new Task!</h2>
-            <input type="text" className="input" onChange={(e)=> {
-                setTodo(e.target.value)
+            <input type="text" className="input" ref={todoElementRef} 
+            onKeyDown={(e) => handleKeyDown(e)} 
+            onChange={(e)=> {setTodo(e.target.value)
             }} />
+            <button className="addButton" onClick={handleAddTodo}>Add</button>
         </div> 
     );
 }
